1485b7c8e4967f458325f38c588805c5cdb0a004
Brian Rowe <browe@rentpath.com>
2015-07-23 12:26:55 -0400

    Query rules are in the query namespace.

--- src/code_analysis/core.clj
+++ src/code_analysis/core.clj
@@ -5,17 +5,6 @@
 (def connection (d/connect "datomic:free://localhost:4334/message-api"))
 (def db (d/db connection))
 
-(def rules
- '[[(node-files ?n ?f) [?n :node/object ?f] [?f :git/type :blob]]
-   [(node-files ?n ?f) [?n :node/object ?t] [?t :git/type :tree]
-                       [?t :tree/nodes ?n2] (node-files ?n2 ?f)]
-   [(object-nodes ?o ?n) [?n :node/object ?o]]
-   [(object-nodes ?o ?n) [?n2 :node/object ?o] [?t :tree/nodes ?n2] (object-nodes ?t ?n)]
-   [(commit-files ?c ?f) [?c :commit/tree ?root] (node-files ?root ?f)]
-   [(commit-codeqs ?c ?cq) (commit-files ?c ?f) [?cq :codeq/file ?f]]
-   [(file-commits ?f ?c) (object-nodes ?f ?n) [?c :commit/tree ?n]]
-   [(codeq-commits ?cq ?c) [?cq :codeq/file ?f] (file-commits ?f ?c)]])
-
 (def sha "8d15e962510e5812618893bb80dd457f5d190525")
 
 ;; (defn codeqs
